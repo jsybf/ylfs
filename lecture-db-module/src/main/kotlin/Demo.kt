3d3ec8ca9672127b9183e20f16301e0f8504914e
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-02-06 15:45:47 +0900

    add serialization sample

--- lecture-db-module/src/main/kotlin/Demo.kt
+++ lecture-db-module/src/main/kotlin/Demo.kt
@@ -2,6 +2,11 @@ package io.gitp.ysfl.db
 
 import io.gitp.ysfl.db.payload.DptGroupPayloadVo
 import io.gitp.ysfl.db.payload.DptPayloadVo
+import io.gitp.ysfl.db.response.DptGroupResp
+import io.gitp.ysfl.db.response.DptResp
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonObject
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -43,11 +48,42 @@ fun requestDpt(dptId: String, year: Year, semester: Semester): HttpResponse<Stri
     return resp
 }
 
+private val json = Json { ignoreUnknownKeys = true }
+
+fun decodeDptGroupResponse(respStr: String): List<DptGroupResp> {
+    val postRefined = Json
+        .parseToJsonElement(respStr)
+        .jsonObject["dsUnivCd"]!!
+
+    return json.decodeFromJsonElement<List<DptGroupResp>>(postRefined)
+}
+
+fun decodeDptResponse(respStr: String): List<DptResp> {
+    val postRefined = Json
+        .parseToJsonElement(respStr)
+        .jsonObject["dsFaclyCd"]!!
+
+    return json.decodeFromJsonElement<List<DptResp>>(postRefined)
+}
+
+
 fun main() {
-    val dptGroupResp: HttpResponse<String> = requestDptGroup(Year.of(2024), Semester.FIRST)
-    val dptResp: HttpResponse<String> = requestDpt("s1101", Year.of(2024), Semester.FIRST)
+    // val dptGroupResp: HttpResponse<String> = requestDptGroup(Year.of(2025), Semester.FIRST)
+    // val dptResp: HttpResponse<String> = requestDpt("s1101", Year.of(2025), Semester.FIRST)
+    // val dptGroupRespJson = decodeDptGroupResponse(dptGroupResp.body())
+    // val dptRespJson = decodeDptResponse(dptResp.body())
+    // dptGroupRespJson.forEach { println(it) }
+    // dptRespJson.forEach { println(it) }
+    // dptRespList.forEach { println(it) }
 
-    println(dptGroupResp.body())
-    println(dptResp.body())
-
+    val dptGroupResponse: List<DptGroupResp> = decodeDptGroupResponse(requestDptGroup(Year.of(2025), Semester.FIRST).body())
+    dptGroupResponse
+        .associate { dptGroupResp ->
+            val dptRespStr = requestDpt(dptGroupResp.dptGroupId, Year.of(2025), Semester.FIRST)
+            val dptResp: List<DptResp> = decodeDptResponse(dptRespStr.body())
+            return@associate Pair(dptGroupResp, dptResp)
+        }.onEach { (dptGroup, dpt) ->
+            println(dptGroup)
+            dpt.forEach { println("\t${it}") }
+        }
 }
---
+++ lecture-db-module/src/main/kotlin/response/DptGroupResp.kt
@@ -0,0 +1,12 @@
+package io.gitp.ysfl.db.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DptGroupResp(
+    @SerialName("deptNm")
+    val dptGroupName: String,
+    @SerialName("deptCd")
+    val dptGroupId: String
+)
