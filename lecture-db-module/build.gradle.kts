e3a557a1bc2aa0b5cc40eb4903edae8c033d22d6
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-02-06 20:45:59 +0900

    add dependencies lecture-db-module

--- lecture-db-module/build.gradle.kts
+++ lecture-db-module/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("kotlin-convention")
 }
+
+dependencies {
+    implementation(project(":yonsei-client-module"))
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.mysql.connector.java)
+}
