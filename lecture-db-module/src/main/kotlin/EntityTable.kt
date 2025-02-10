ba818817924e0349002247b16ba6e5207a7c88e2
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-02-10 15:21:02 +0900

    add lecture Entity Table and Repository for insertions

--- lecture-db-module/src/main/kotlin/EntityTable.kt
+++ lecture-db-module/src/main/kotlin/EntityTable.kt
@@ -4,6 +4,7 @@ import io.gitp.ysfl.client.Semester
 import io.gitp.ysfl.db.custom_type.year
 import org.jetbrains.exposed.sql.ColumnTransformer
 import org.jetbrains.exposed.sql.Table
+import java.time.DayOfWeek
 import java.time.Year
 
 
@@ -30,6 +31,73 @@ object DptTbl : Table("dpt") {
 
 }
 
+object LectureTbl : Table("lecture") {
+    val id = integer("lecture_id").autoIncrement()
+    val dptId = varchar("dpt_id", 5) references DptTbl.dptId
+
+    val mainId = char("main_id", 7)
+    val classDivisionId = char("class_division_id", 2)
+    val subId = char("sub_id", 2)
+
+    val name = char("name", 100)
+
+    override val primaryKey = PrimaryKey(id)
+
+    init {
+        index(isUnique = false, mainId, classDivisionId, subId)
+    }
+}
+
+object LocationTbl : Table("location") {
+    val id = integer("location_id").autoIncrement()
+    val type = varchar("type", 16).check { it inList listOf("REAL_TIME_ONLINE", "ONLINE", "OFFLINE") }
+
+    // OffLine columns
+    val offlineBuilding = varchar("offline_building", 10).nullable()
+    val offlineAddress = varchar("offline_address", 10).nullable()
+
+    // Online columns
+    val duplicateCapability = bool("duplicate_capability").nullable()
+}
+
+object ProfessorTbl : Table("professor") {
+    val id = integer("professor_id").autoIncrement()
+    val name = varchar("name", 30)
+
+    override val primaryKey = PrimaryKey(id)
+}
+
+object ScheduleTbl : Table("schedule") {
+    val id = integer("schedule_id").autoIncrement()
+    val day = varchar("day", 9).check { it inList listOf("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY") }
+        .transform(DayOfWeekTransform())
+    val period = integer("periods").check { it.greaterEq(-1) }
+}
+
+
+object LectureSchedLocTbl : Table("lecture_sched_loc") {
+    val lectureId = integer("lecture_id") references LectureTbl.id
+    val scheduleId = integer("schedule_id") references ScheduleTbl.id
+    val locationId = integer("location_id") references LocationTbl.id
+
+    // 인덱스
+    init {
+        index(isUnique = false, lectureId)
+    }
+}
+
+// LectureProfessors 테이블
+object LectureProfessors : Table() {
+    val lectureId = integer("lecture_id") references LectureTbl.id
+    val professorId = integer("professor_id") references ProfessorTbl.id
+
+    // 복합 인덱스 설정
+    init {
+        index(isUnique = false, lectureId, professorId)
+    }
+}
+
+
 private class YearTransform() : ColumnTransformer<String, Year> {
     override fun unwrap(value: Year): String = value.toString()
 
@@ -40,5 +108,12 @@ private class SemesterTransform() : ColumnTransformer<String, Semester> {
     override fun unwrap(value: Semester): String = value.name
 
     override fun wrap(value: String): Semester = Semester.valueOf(value)
+
+}
+
+private class DayOfWeekTransform() : ColumnTransformer<String, DayOfWeek> {
+    override fun unwrap(value: DayOfWeek): String = value.name
+
+    override fun wrap(value: String): DayOfWeek = DayOfWeek.valueOf(value)
 
 }
