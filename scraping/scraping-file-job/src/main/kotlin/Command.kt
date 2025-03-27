cb514a1bfa833254bbfc00e16c808f37cc75595b
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-03-27 10:39:10 +0900

    use Semester from entity subproject

--- scraping/scraping-file-job/src/main/kotlin/Command.kt
+++ scraping/scraping-file-job/src/main/kotlin/Command.kt
@@ -6,21 +6,14 @@ import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.int
+import io.gitp.ylfs.entity.type.Semester
 import java.nio.file.Path
 import java.time.Year
-
-enum class Semester(val num: Int) {
-    FIRST(1), SECOND(2);
-
-    companion object {
-        fun byNum(num: Int): Semester? = entries.find { it.num == num }
-    }
-}
 
 class ScapeThenSaveFileCommand() : CliktCommand() {
     // crawl text --year 2024 --semester 1 --path ./data/24-1 --target 4
     val year: Year by option("-y", "--year").int().convert { Year.of(it) }.required().help { "ex: 2023" }
-    val semester: Semester by option("-s", "--semester").int().convert { Semester.byNum(it)!! }.required().help { "ex: 1 or 2" }
+    val semester: Semester by option("-s", "--semester").int().convert { Semester.codeof(it)!! }.required().help { "ex: 10 or 20" }
     val path: Path by option("-p", "--path").convert { Path.of(it) }.required().help { "files save base path. relative, absolte is both allowed" }
     val target: Int by option("-t", "--target").int().required().help { "scraping util certain level.  1: college,2: department,3: lecture 4: mileage" }
     override fun run() {
