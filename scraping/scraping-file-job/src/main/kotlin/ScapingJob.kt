a8f8fa7e4a0c1df5dd0249bc0015c2897bb1109b
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-03-28 03:17:11 +0900

    complete

--- scraping/scraping-file-job/src/main/kotlin/ScapingJob.kt
+++ scraping/scraping-file-job/src/main/kotlin/ScapingJob.kt
@@ -10,120 +10,128 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import org.slf4j.LoggerFactory
 import java.nio.file.Path
 import java.time.Year
 import kotlin.io.path.createDirectories
 import kotlin.io.path.writeText
 
-private fun requestColleage(collegeReq: CollegeRequest): Deferred<CollegeResponse> = CollegeClient
-    .request(collegeReq.toPayload())
-    .thenApplyAsync { respResult: Result<String> -> CollegeResponse(collegeReq, Json.decodeFromString(respResult.getOrThrow())) }
-    .asDeferred()
-
-private fun requestDpt(dptReq: DptRequest): Deferred<DptResponse> {
-    val dptRespAsync = DptClient
-        .request(dptReq.toPayload())
-        .thenApplyAsync { respResult: Result<String> -> DptResponse(dptReq, Json.decodeFromString(respResult.getOrThrow())) }
+private object RequestFuncs {
+    fun requestColleage(collegeReq: CollegeRequest): Deferred<CollegeResponse> = CollegeClient
+        .request(collegeReq.toPayload())
+        .thenApplyAsync { respResult: Result<String> -> CollegeResponse(collegeReq, Json.decodeFromString(respResult.getOrThrow())) }
         .asDeferred()
-    return dptRespAsync
 
+    fun requestDpt(dptReq: DptRequest): Deferred<DptResponse> {
+        val dptRespAsync = DptClient
+            .request(dptReq.toPayload())
+            .thenApplyAsync { respResult: Result<String> -> DptResponse(dptReq, Json.decodeFromString(respResult.getOrThrow())) }
+            .asDeferred()
+        return dptRespAsync
+
+    }
+
+    fun requestLecture(lectureReq: LectureRequest): Deferred<LectureResponse> {
+        val dptRespAsync = LectureClient
+            .request(lectureReq.toPayload())
+            .thenApplyAsync { respResult: Result<String> -> LectureResponse(lectureReq, Json.decodeFromString(respResult.getOrThrow())) }
+            .asDeferred()
+        return dptRespAsync
+    }
+
+    fun requestMlgInfo(mlgReq: MlgRequest): Deferred<MlgInfoResponse> {
+        val dptRespAsync = MlgInfoClient
+            .request(mlgReq.toMlgInfoPayload())
+            .thenApplyAsync { respResult: Result<String> -> MlgInfoResponse(mlgReq, Json.decodeFromString(respResult.getOrThrow())) }
+            .asDeferred()
+        return dptRespAsync
+    }
+
+
+    fun requestMlgRank(mlgReq: MlgRequest): Deferred<MlgRankResponse> {
+        val dptRespAsync = MlgRankClient
+            .request(mlgReq.toMlgRankPayload())
+            .thenApplyAsync { respResult: Result<String> -> MlgRankResponse(mlgReq, Json.decodeFromString(respResult.getOrThrow())) }
+            .asDeferred()
+        return dptRespAsync
+    }
 }
 
-private fun requestLecture(lectureReq: LectureRequest): Deferred<LectureResponse> {
-    val dptRespAsync = LectureClient
-        .request(lectureReq.toPayload())
-        .thenApplyAsync { respResult: Result<String> -> LectureResponse(lectureReq, Json.decodeFromString(respResult.getOrThrow())) }
-        .asDeferred()
-    return dptRespAsync
+private object ResponseParsers {
+    fun parseCollegeResponse(collegeResp: CollegeResponse): List<DptRequest> =
+        collegeResp.resp
+            .jsonObject["dsUnivCd"]!!
+            .jsonArray
+            .map { jsonObj ->
+                val collegeCode = jsonObj.jsonObject["deptCd"]!!.jsonPrimitive.content
+                DptRequest(collegeResp.request.year, collegeResp.request.semester, collegeCode)
+            }
+
+    fun parseDptRsponse(dptResp: DptResponse): List<LectureRequest> =
+        dptResp.resp
+            .jsonObject["dsFaclyCd"]!!
+            .jsonArray
+            .map { jsonObj ->
+                val dptCode = jsonObj.jsonObject["deptCd"]!!.jsonPrimitive.content
+                LectureRequest(dptResp.request.year, dptResp.request.semester, dptResp.request.collegeCode, dptCode)
+            }
+
+    fun parseLectureResponse(lectureResp: LectureResponse): List<MlgRequest> =
+        lectureResp.resp
+            .jsonObject["dsSles251"]!!
+            .jsonArray
+            .map { jsonObj ->
+                val lecturecode = LectureCode(
+                    mainCode = jsonObj.jsonObject["subjtnb"]!!.jsonPrimitive.content,
+                    classCode = jsonObj.jsonObject["corseDvclsNo"]!!.jsonPrimitive.content,
+                    subCode = jsonObj.jsonObject["prctsCorseDvclsNo"]!!.jsonPrimitive.content,
+                )
+                MlgRequest(lectureResp.request.year, lectureResp.request.semester, lectureResp.request.collegeCode, lectureResp.request.dptCode, lecturecode)
+            }
 }
 
-private fun requestMlgInfo(mlgReq: MlgRequest): Deferred<MlgInfoResponse> {
-    val dptRespAsync = MlgInfoClient
-        .request(mlgReq.toMlgInfoPayload())
-        .thenApplyAsync { respResult: Result<String> -> MlgInfoResponse(mlgReq, Json.decodeFromString(respResult.getOrThrow())) }
-        .asDeferred()
-    return dptRespAsync
-}
-
-
-private fun requestMlgRank(mlgReq: MlgRequest): Deferred<MlgRankResponse> {
-    val dptRespAsync = MlgRankClient
-        .request(mlgReq.toMlgRankPayload())
-        .thenApplyAsync { respResult: Result<String> -> MlgRankResponse(mlgReq, Json.decodeFromString(respResult.getOrThrow())) }
-        .asDeferred()
-    return dptRespAsync
-}
-
-private fun parseCollegeResponse(collegeResp: CollegeResponse): List<DptRequest> =
-    collegeResp.resp
-        .jsonObject["dsUnivCd"]!!
-        .jsonArray
-        .map { jsonObj ->
-            val collegeCode = jsonObj.jsonObject["deptCd"]!!.jsonPrimitive.content
-            DptRequest(collegeResp.request.year, collegeResp.request.semester, collegeCode)
-        }
-
-private fun parseDptRsponse(dptResp: DptResponse): List<LectureRequest> =
-    dptResp.resp
-        .jsonObject["dsFaclyCd"]!!
-        .jsonArray
-        .map { jsonObj ->
-            val dptCode = jsonObj.jsonObject["deptCd"]!!.jsonPrimitive.content
-            LectureRequest(dptResp.request.year, dptResp.request.semester, dptResp.request.collegeCode, dptCode)
-        }
-
-private fun parseLectureResponse(lectureResp: LectureResponse): List<MlgRequest> =
-    lectureResp.resp
-        .jsonObject["dsSles251"]!!
-        .jsonArray
-        .map { jsonObj ->
-            val lecturecode = LectureCode(
-                mainCode = jsonObj.jsonObject["subjtnb"]!!.jsonPrimitive.content,
-                classCode = jsonObj.jsonObject["corseDvclsNo"]!!.jsonPrimitive.content,
-                subCode = jsonObj.jsonObject["prctsCorseDvclsNo"]!!.jsonPrimitive.content,
-            )
-            MlgRequest(lectureResp.request.year, lectureResp.request.semester, lectureResp.request.collegeCode, lectureResp.request.dptCode, lecturecode)
-        }
 
 private fun saveToFile(filePath: Path, content: String) {
     filePath.writeText(content)
 }
 
-private fun buildJsonArr(jsonObjStrList: List<String>): String = buildString {
-    append("[")
-    for (i in jsonObjStrList.indices) {
-        append(jsonObjStrList[i])
-        if (i != jsonObjStrList.size - 1) append(",")
+private val logger = LoggerFactory.getLogger(object {}::class.java)
+suspend fun job(year: Year, semester: Semester, basePath: Path, delayMs: Long = 4) {
 
-    }
-    append("]")
-}
-
-suspend fun job(year: Year, semester: Semester, basePath: Path, delay: Long = 4) {
     basePath.createDirectories()
 
-    val collegeResp: CollegeResponse = requestColleage(CollegeRequest(year, semester)).await()
+    /* college */
+    logger.info("scraping college")
+    val collegeResp: CollegeResponse = RequestFuncs.requestColleage(CollegeRequest(year, semester)).await()
     saveToFile(basePath.resolve("college.json"), Json.encodeToString(collegeResp))
 
 
-    val dptReqList = parseCollegeResponse(collegeResp)
-    val dptRespList = dptReqList.map { requestDpt(it) }.awaitAll()
+    /* deparment */
+    logger.info("scraping department")
+    val dptReqList = ResponseParsers.parseCollegeResponse(collegeResp)
+    val dptRespList = dptReqList.map { RequestFuncs.requestDpt(it) }.awaitAll()
     saveToFile(basePath.resolve("dpt.json"), Json.encodeToString(dptRespList))
 
-    val lectureReqList = dptRespList.map(::parseDptRsponse).flatten()
-    val lectureRespList = lectureReqList.map { requestLecture(it) }.awaitAll()
+    /* lecture */
+    logger.info("scraping lecture")
+    val lectureReqList = dptRespList.map { ResponseParsers.parseDptRsponse(it) }.flatten()
+    val lectureRespList = lectureReqList.map { RequestFuncs.requestLecture(it) }.awaitAll()
     saveToFile(basePath.resolve("lecture.json"), Json.encodeToString(lectureRespList))
 
-    val mlgReqList = lectureRespList.map(::parseLectureResponse).flatten()
+    /* mileage info */
+    logger.info("scraping mileage info")
+    val mlgReqList = lectureRespList.map { ResponseParsers.parseLectureResponse(it) }.flatten()
     val mlgInfoRespList = mlgReqList.map {
-        delay(delay)
-        requestMlgInfo(it)
+        delay(delayMs)
+        RequestFuncs.requestMlgInfo(it)
     }.awaitAll()
     saveToFile(basePath.resolve("mlg-info.json"), Json.encodeToString(mlgInfoRespList))
 
+    /* milage rank */
+    logger.info("scraping mileage rank")
     val mlgRankRespList = mlgReqList.map {
-        delay(delay)
-        requestMlgRank(it)
+        delay(delayMs)
+        RequestFuncs.requestMlgRank(it)
     }.awaitAll()
     saveToFile(basePath.resolve("mlg-rank.json"), Json.encodeToString(mlgRankRespList))
 
